05e045b198a666703e192f52f05d1d967183be6d
Waterlilyrr <noemict94@hotmail.it>
2025-03-11 22:58:57 +0100

    feat: add ProductCardSkeleton

--- src/app/(customerFacing)/page.tsx
+++ src/app/(customerFacing)/page.tsx
@@ -1,4 +1,4 @@
-import ProductCard from "@/components/ProductCard/ProductCard";
+import { ProductCard } from "@/components/ProductCard/ProductCard";
 import { Button } from "@/components/ui/button";
 import db from "@/db/db";
 import { Product } from "@prisma/client";
