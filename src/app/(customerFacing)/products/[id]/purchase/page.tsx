591c8e3a711eb40c1397b72290ce6eaad713132f
NoemiFazio <noemict94@hotmail.it>
2025-03-14 22:47:45 +0100

    feat: implement stripe in PurchasePage

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,5 +1,9 @@
 import db from "@/db/db";
 import { notFound } from "next/navigation";
+import Stripe from "stripe";
+
+// this musst occur on the server, not on the client, because we are going to pass a secret key. Here we are "casting" in order to remove ts error
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export default async function PurchasePage({
   params: { id },
@@ -10,5 +14,21 @@ export default async function PurchasePage({
     where: { id },
   });
   if (product == null) return notFound();
-  return <h1>Hi!</h1>;
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: "USD",
+    //* metadata: useful to tie a purchase to a particular product
+    metadata: {
+      productId: product.id,
+    },
+  });
+
+  //* the client_secret is used on the client to specify the paymentIntent  we are working on
+  if (paymentIntent.client_secret == null) {
+    throw Error("Stripe failed to create payment intent");
+  }
+
+  //   return <CheckoutForm product={product} clientSecret={paymentIntent.client_secret}></CheckoutForm>;
+  return <></>;
 }
